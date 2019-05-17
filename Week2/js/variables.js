b8a3b3ff8942c924463d5be6376d30dc30a9171c
Eric Poitras <EricPoitras@users.noreply.github.com>
2019-05-17 13:54:26 -0600

    Add files via upload

---
+++ Week2/js/variables.js
@@ -0,0 +1,12 @@
+/* 
+
+Example of multi-line comments syntax in JS:
+https://www.w3schools.com/js/js_comments.asp
+
+Single line comments can be added via "//"
+
+*/
+
+// Debug your code by printing information in the console. Open the console in your browser's developer tool. For additional options, see https://www.w3schools.com/js/js_output.asp
+
+console.log("Hello World!");
