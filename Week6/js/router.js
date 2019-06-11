217b4de24a09b9ba6cfcf2d708e2a194f95db0c5
Eric Poitras <EricPoitras@users.noreply.github.com>
2019-06-11 10:58:18 -0600

    Add files via upload

---
+++ Week6/js/router.js
@@ -0,0 +1,28 @@
+// https://www.w3schools.com/js/js_switch.asp
+
+// define the router as a function that receives a value. Use the function keyword and an identifier using a value such as router
+function router(value){
+    
+    switch(value) {
+      case "view_landing":
+        // code block
+            console.log("Show the landing UI view");
+        break;
+      case "view_loading":
+            console.log("Show the loading UI view");      
+        break;
+      case "view_forecast":
+        // code block
+            console.log("Show the forecast UI view");
+        break;
+      case "view_settings":
+        // code block
+            console.log("Show the settings UI view");
+        break;
+      default:
+        // code block
+            console.log("Error: The requested view is not recognized by the router");
+    }
+    
+}
+
